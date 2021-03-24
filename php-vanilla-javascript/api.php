29f9405d42d29449ca2f07b777239c746b141525
Pete Williamson <pete@willshawmedia.com>
2021-03-24 15:24:59 +0000

    php vanilla js: remove query params code from hash php

--- php-vanilla-javascript/api.php
+++ php-vanilla-javascript/api.php
@@ -9,45 +9,14 @@ $ini_array = parse_ini_file('./api.ini');
 $api_key = $ini_array['key'];
 $api_secret = $ini_array['secret'];
 
-/**
- * build a hash an return as the response
- */
-
+// get the current unix timestamp
 $timestamp = time();
 
 // create a unique nonce
 $nonce = uniqid();
 
-// the message is made up of the order/filter etc params
-// params need to be put into alphabetical order
-$current_message = '';
-// $query_params = $_GET;
-// $struct_params = {};
-
-// make a simple struct of key/value pairs
-// query_params.each(function(param){
-    // check if the key is not disabled
-    // if( !param.disabled ) {
-        // struct_params[ param.key ] = param.value;
-    // }
-// });
-
-// make an ordered array of key names
-// $arr_param_names = Object.keys( struct_params ).sort();
-
-// add the params in order to the message
-// arr_param_names.each(function(param){
-    // get the value, blank if it's null
-    // $val = struct_params[ param ]||'';
-    // if the value is not blank, add it to the message
-    // if( val !== '' ) {
-        // current_message+=param+'='+val;
-    // }
-// });
-
 // concatenate data together
 $combined_data = $api_key
-                . $current_message
                 . $api_secret
                 . $nonce
                 . $timestamp;
